b52744c576216aec55e58a8945c0da2a2f49664d
Sheetal <sheetalsingh7007@gmail.com>
2023-02-05 22:37:29 -0600

    completed challenge 7

--- index.js
+++ index.js
@@ -184,7 +184,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [firstGame, secondGame, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topGameName = document.createElement("p");
+topGameName.innerText = firstGame.name;
+firstGameContainer.appendChild(topGameName);
 
 // do the same for the runner up item
+const secondGameName = document.createElement("p");
+secondGameName.innerText = secondGame.name;
+secondGameContainer.appendChild(secondGameName);
